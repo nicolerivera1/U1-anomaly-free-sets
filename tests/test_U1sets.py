82af6ed868e7863a9ba37896bf1484f1952a5308
Nicole Rivera <41299759+nicolerivera1@users.noreply.github.com>
2022-09-27 11:20:19 -0500

    feat: first tests

--- tests/test_U1sets.py
+++ tests/test_U1sets.py
@@ -1,1 +1,10 @@
-#write tests
+import unittest
+
+class Test_hello(unittest.TestCase):
+    def test__working(self):
+        self.assertEqual(1,
+                         1, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
