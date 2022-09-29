557e8330aefe21cd659757726db3f3c492940e70
nicole rivera <41299759+nicolerivera1@users.noreply.github.com>
2022-09-29 14:01:52 -0500

    update version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def main():
         name="U1-anomaly-free-sets",
 
         # Version number:
-        version="0.0.2",
+        version="0.0.3",
 
         # Application author details:
         author="nicolerivera1",
